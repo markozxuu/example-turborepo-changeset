7e44895996e5192f8eb14ef90489f84fdabc4fcd
Markoz Pena <markdrew53@gmail.com>
2023-08-02 06:05:50 -0600

    feat: expose banner component

--- packages/acme-core/src/index.tsx
+++ packages/acme-core/src/index.tsx
@@ -1,4 +1,5 @@
 export { Button, type ButtonProps } from "./Button";
 export { Title, type TitleProps } from "./Title";
 export { Container, type ContainerProps  } from "./Container";
+export { Banner, type BannerProps } from './Banner';
 
