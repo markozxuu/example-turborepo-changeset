b4d598cca1c3666bcfd6dfa91acf3f808b2f8c28
Markoz Pena <markdrew53@gmail.com>
2023-08-02 10:41:53 -0600

    feat: add checbox component export

--- packages/acme-core/src/index.tsx
+++ packages/acme-core/src/index.tsx
@@ -2,4 +2,5 @@ export { Button, type ButtonProps } from "./Button";
 export { Title, type TitleProps } from "./Title";
 export { Container, type ContainerProps  } from "./Container";
 export { Banner, type BannerProps } from './Banner';
+export { Checkbox } from './Checkbox'
 
